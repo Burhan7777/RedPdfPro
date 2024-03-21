34b6e658bd85aaff4ad47989424702860ff99fdf
peerzadaburhan <peerzadaburhan7@gmail.com>
2024-03-21 13:00:45 +0530

    Adds new button to select pdf in merge pdf screen

--- app/src/main/java/com/pzbdownloaders/redpdfpro/mergepdffeature/screens/MergePdfScreen.kt
+++ app/src/main/java/com/pzbdownloaders/redpdfpro/mergepdffeature/screens/MergePdfScreen.kt
@@ -3,8 +3,10 @@ package com.pzbdownloaders.redpdfpro.mergepdffeature.screens
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -19,6 +21,7 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -31,13 +34,19 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
 import com.chaquo.python.Python
 import com.pzbdownloaders.redpdfpro.MainActivity
@@ -79,6 +88,11 @@ fun MergePdf(activity: MainActivity, viewModel: MyViewModel) {
     var context = LocalContext.current
     var scope = rememberCoroutineScope()
 
+    val stroke = Stroke(
+        width = 2f,
+        pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+    )
+
     Box(modifier = Modifier.fillMaxSize()) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -117,14 +131,41 @@ fun MergePdf(activity: MainActivity, viewModel: MyViewModel) {
                     }
                 }
             }
-            Button(
-                onClick = { result.launch("application/pdf") },
-                modifier = Modifier.padding(top = 30.dp)
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp)
+                    .padding(start = 20.dp, end = 20.dp)
+                    .clickable {
+                        result.launch("application/pdf")
+                    }
+                    .drawBehind {
+                        drawRoundRect(
+                            color = Color.Red,
+                            style = stroke,
+                            cornerRadius = CornerRadius(10.dp.toPx())
+                        )
+                    },
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.Transparent
+                ),
             ) {
+                Image(
+                    painter = painterResource(id = R.drawable.upload),
+                    contentDescription = stringResource(
+                        id = R.string.upload
+                    ),
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(top = 20.dp)
+                )
                 Text(
                     text = stringResource(id = R.string.addPDF),
-
-                    )
+                    fontSize = 20.sp,
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(top = 10.dp)
+                )
             }
         }
 
